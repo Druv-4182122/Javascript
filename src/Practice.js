f1ffd5b142a7e5dbb3357bbca851dff0c7457f0a
Druv-4182122 <druvnagpal@gmail.com>
2025-02-04 18:50:30 +0530

    added and configured vite, nodemodules

--- src/Practice.js
+++ src/Practice.js
@@ -1,5 +1,8 @@
+//"use strict"; used to treat all js code as new version of js
 const accname = "Druv"
 var accNo = 23
 let accEmail = "abc@gmail.com"
 
 console.table({accname, accNo, accEmail})
+console.log(`My name is ${accname} and my account number is ${accNo}`)
+// alert(3+3) cannot run in node, can run in browser
